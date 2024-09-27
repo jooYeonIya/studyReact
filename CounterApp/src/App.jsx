072696951236a1dd324d626d456716dc21936c04
jooyeoniya <jooyeonstudyios@gmail.com>
2024-09-27 12:05:08 +0900

    조건에 따라 실행하도록 함

--- CounterApp/src/App.jsx
+++ CounterApp/src/App.jsx
@@ -1,43 +1,31 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/Header';
-import Func3, { Func1, Func2 } from './components/Components';
-import ComponentObj from './components/ComponentObj';
+import CondComponent from './components/CondComponent';
+import ShareButton from './components/ShareButton';
+
 
 function App() {
-  let [todo, setTodo] = useState("");
-  let [todoList, setTodoList] = useState([]);
-
-  const changeTodo = (event) => {
-    setTodo(event.target.value)
+  let [cond, setCond] = useState(false);
+  let changeStatus = () => {
+    setCond(!cond);
   }
 
-  const addTodoList = (event) => {
-    event.preventDefault();
-    if (!todo) return;
-    setTodo("");
-    setTodoList((arr) => [...arr, todo]);
-    console.log("내부" + todoList);
-    // React의 상태 업데이트가 비동기적으로 처리되기 때문
-    // setState 함수 (setTodoList처럼)를 호출할 때 상태가 즉시 업데이트되지 않음
-    // setTodoList는 비동기적으로 동작하므로, todoList는 아직 업데이트된 상태를 반영하지 않음
-    // 즉, setTodoList 직후에 출력되는 todoList는 업데이트 전의 상태를 출력
-    // 투두리스트에 반영되는 시점은 setTodoList 함수의 실행이 끝난 즉시가 아니라, React가 상태 변경을 감지하고 컴포넌트가 다시 렌더링된 후
-    // setTodoList와 setTodo에 의해 상태가 변경된 것을 감지하고 컴포넌트를 리렌더링 -> 렌더링이 끝나면 투두리스트 업데이트가 반영됨
-  }
-
-  console.log("외부" + todoList);
+  let style = () => {
+    return cond 
+      ? {backgroundColor: "red", color: "green"}
+      : {backgroundColor: "blue", color: "orange"}
+  }  
+  
   return (
     <>
-      <Func1 />
-      <Func2 />
-      <Func3 />
-      <ComponentObj />
-      <Header />
-      <input type="text" value={todo} placeholder="할 일을 입력하세요" onChange={changeTodo} />
-      <button onClick={addTodoList}>Add</button>
-      <hr />
-      {todoList.map(todo => <li>{todo}</li>)}
+      {cond ? <CondComponent /> : null}
+      {cond ? "참" : "거짓"}
+
+      <br/>
+
+      <ShareButton text={cond ? "로그아웃" : "로그인"} 
+                         style={style()}
+                         showAlert={changeStatus}/>
     </>
   )
 }
