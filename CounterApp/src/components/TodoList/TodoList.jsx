93c888b3d80defb2ad494fd4d9a26dfa32e44539
jooyeoniya <jooyeonstudyios@gmail.com>
2024-09-27 16:10:54 +0900

    todoList: Todo 검색 기능 추가

--- CounterApp/src/components/TodoList/TodoList.jsx
+++ CounterApp/src/components/TodoList/TodoList.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import TodoItem from "./TodoItem";
 
 export default function TodoList({todo}) {
+    let [search, setSearch] = useState("")
+    let changeSearch = (e) => {
+        setSearch(e.target.value);
+    }
+
+    let getSearchResult = () => {
+        return search === "" 
+        ? todo
+        : todo.filter((it) => it.content.includes(search))
+
+    }
     return (
         <>
             <h4>Todo List</h4>
-            <input type="text" placeholder="검색어를 입력하세요" />
+            <input type="text" placeholder="검색어를 입력하세요" value={search} onChange={changeSearch}/>
             <br />
-            {todo.map ((it) => <div><TodoItem key={it.id} it={it}/></div>)}
+            {getSearchResult().map ((it) => <div><TodoItem key={it.id} it={it}/></div>)}
         </>
     )
 }
