97017f43a70e3c52d2d4e0cbfff6e5ea9d070cf9
jooyeoniya <jooyeonstudyios@gmail.com>
2024-09-27 15:56:59 +0900

    todoList: Todo Create 기능 추가 (화면 표시 미반영)

--- CounterApp/src/App.jsx
+++ CounterApp/src/App.jsx
@@ -1,16 +1,36 @@
 import { useState } from 'react'
+import { useRef } from 'react'
 import './App.css'
 import Header from './components/TodoList/Header'
 import TodoEditor from './components/TodoList/TodoEditor'
 import TodoList from './components/TodoList/TodoList'
 
-
 function App() {
+  const [todo, setTodo] = useState([{
+    id: 0,
+    isDone: false,
+    content: "리액트 공부하기",
+    date: new Date().getDate()
+  }])
+
+  let idRef = useRef(1);
+
+  const createTodo = (content) => {
+    const newTodoItem = {
+      id: idRef.current,
+      isDone: false,
+      content,
+      date: new Date().getDate()
+    }
+
+    setTodo([...todo, newTodoItem]);
+    idRef += 1; 
+  }
 
   return (
     <>
       <Header />
-      <TodoEditor />
+      <TodoEditor onCreate={createTodo}/>
       <TodoList />
     </>
   )
--- CounterApp/src/components/TodoList/TodoEditor.jsx
+++ CounterApp/src/components/TodoList/TodoEditor.jsx
@@ -1,9 +1,20 @@
-export default function TodoEditor() {
+import { useState } from "react"
+
+export default function TodoEditor({onCreate}) {
+    let [content, setContent] = useState("");
+    let changeContent = (e) => {
+        setContent(e.target.value);
+    }
+    
+    let submit = () => {
+        onCreate(content);
+    }
+
     return (
         <>
             <h4>새로운 Todo 작성하기</h4>
-            <input type="text" placeholder="새로운 Todo..."/>
-            <button>추가</button>
+            <input type="text" placeholder="새로운 Todo..." value={content} onChange={changeContent}/>
+            <button onClick={submit}>추가</button>
         </>
     )
 }
