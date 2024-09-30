f0a7106f4f33ca97b1a802e0fe4421bb592cd168
jooyeoniya <jooyeonstudyios@gmail.com>
2024-09-30 09:53:17 +0900

    movieApp: api 불러오기

--- moiveApp/src/App.jsx
+++ moiveApp/src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year")
+    .then((reponse) => reponse.json())
+    .then((json) => {
+      setMovies(json.data.movies);
+      setIsLoading(false);
+    })
+  }, []);
   
   return (
     <>
       <h1>movie App</h1>
+      {isLoading ? "로딩 중" : null}
     </>
   )
 }
