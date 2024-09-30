5bf8b0cfee3350504d739f73cf0aac14f6d9b366
jooyeoniya <jooyeonstudyios@gmail.com>
2024-09-30 11:22:52 +0900

    movieApp: api list 화면에 보여주기 (ul li 태그 장르 확인해 보기-> 다른 태그로는 잘 나왔음)

---
+++ moiveApp/src/AppMovieList.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Movie from '../components/Movie';
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async() => {
+    const reponse = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year");
+    const json = await reponse.json();
+    setMovies(json.data.movies);
+    setIsLoading(false);
+  }
+
+    // async function getMovies() {  }
+
+  // useEffect(() => {
+  //   fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year")
+  //   .then((reponse) => reponse.json())
+  //   .then((json) => {
+  //     setMovies(json.data.movies);
+  //     setIsLoading(false);
+  //   })
+  // }, []);
+
+  useEffect(() => { getMovies(); }, []);
+
+  return (
+    <>
+      <h1>movie App</h1>
+      {isLoading ? "로딩 중" : 
+                    <div>
+                      {movies.map((movie) => (<Movie key={movie.id} movie={movie}/>))}
+                    </div>
+      }
+    </>
+  )
+}
+
+export default App
