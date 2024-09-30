8e9500d0afae4c796141c36009e173f1c05cb89d
jooyeoniya <jooyeonstudyios@gmail.com>
2024-09-30 09:33:38 +0900

    movieApp: 초기 설정

---
+++ moiveApp/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  
+  return (
+    <>
+      <h1>movie App</h1>
+    </>
+  )
+}
+
+export default App
