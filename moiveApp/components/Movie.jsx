906a9524b5fbddeb85e1ea8c376b20726bab3ead
jooyeoniya <jooyeonstudyios@gmail.com>
2024-10-01 09:29:09 +0900

    movieApp: api list 화면에 보여주기 - 장르 보여주기 수정 -

--- moiveApp/components/Movie.jsx
+++ moiveApp/components/Movie.jsx
@@ -1,28 +1,18 @@
-export default function Movie({movie}) {
+export default function Movie({ movie }) {
     return (
-    <>
-        <div>
-            <img src={movie.large_cover_image} alt="" />
-            <h2>{movie.title}</h2>
-            <p>{movie.summary}</p>
-            <p>Genres: {movie.genres ? movie.genres.join(", ") : "장르 정보 없음"}</p>
-
-            {/* <ul>
-                {movie.genres.map((genre, index) => (
-                    <li key={index}>{genre}</li>
-                ))}
-            </ul> */}
-                   <p>
-                    {movie.genres && movie.genres.length > 0 ? (
-                        movie.genres.map((genre, index) => (
-                            <span key={index}>{genre} </span>
-                        ))
-                    ) : (
-                        "장르 정보 없음"
-                    )}
-                </p>
-        </div>  
-    </>
+        <>
+            <div>
+                <img src={movie.large_cover_image} alt="" />
+                <h2>{movie.title}</h2>
+                <p>{movie.summary}</p>
+                <p>Genres: {movie.genres ? movie.genres.join(", ") : "장르 정보 없음"}</p>
+                <ul>
+                    {movie.genres && movie.genres.map((genre, index) => (
+                        <li key={index}>{genre}</li>
+                    ))}
+                </ul>
+            </div>
+        </>
     )
 }
 
